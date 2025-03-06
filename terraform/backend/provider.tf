22085c275efe8fb8fc2033382205b49aaedca0ae
kandimallanikhil <52294077+kandimallanikhil@users.noreply.github.com>
2025-03-06 16:50:11 +0530

    Update provider.tf

--- terraform/backend/provider.tf
+++ terraform/backend/provider.tf
@@ -7,7 +7,7 @@ terraform {
     bucket         = "demo-terraform-eks-state-s3-bucket-eu-north-1"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
-   # dynamodb_table = "terraform-eks-state-locks"
+    dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
 }
