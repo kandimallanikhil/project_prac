f4cd512621fa22b277714c99924e9e149d8ec50b
kandimallanikhil <52294077+kandimallanikhil@users.noreply.github.com>
2025-03-10 12:15:11 +0530

    Add files via upload

---
+++ terraform/Provider/s3_variable.tf
@@ -0,0 +1,28 @@
+variable "region_1"{
+    description = "aws_region"
+    type = string
+}
+variable "region_2"{
+    description = "aws_region"
+    type = string
+}
+variable "bucket_name_1" {
+  type        = string
+  description = "Test bucket"
+ # default     = "my-demo-test-bucket"
+}
+variable "bucket_name_2" {
+  type        = string
+  description = "Test bucket"
+ # default     = "my-demo-test-bucket"
+}
+variable "Bucket_1" {
+  type        = string
+  description = "Test bucket"
+ # default     = "my-demo-test-bucket"
+}
+variable "Bucket_2" {
+  type        = string
+  description = "Test bucket"
+ # default     = "my-demo-test-bucket"
+}
---
+++ terraform/Provider/s3_variable.tfvars
@@ -0,0 +1,6 @@
+region_1     = "us-west-1"
+region_2     = "us-east-1"
+bucket_name_1 = "my-demo-bucket-1"
+bucket_name_2 = "my-demo-bucket-2"
+Bucket_1 = "Bucket_1"
+Bucket_2 = "Bucket_2"
